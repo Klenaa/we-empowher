7d488060b2c705bc22939dd8da85fac768cbfdcb
KLeennaa <klena.cheam@gmail.com>
2024-05-26 22:26:28 +0200

    set config

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
+st.set_page_config(page_title="We Empowher", page_icon=":sparkles:", layout="centered", initial_sidebar_state="auto", menu_items=None)
 st.title("Dashboard")
 
 df = pd.read_csv("extract_mail.csv")
