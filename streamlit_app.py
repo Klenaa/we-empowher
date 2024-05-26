e12f5155208ce8ea5f8a374bbf5a8d863172d189
KLeennaa <klena.cheam@gmail.com>
2024-05-26 22:17:31 +0200

    add csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
-import streamlit as st
+import streamlit as st 
 import pandas as pde
 import numpy as np
 
 st.title("Dashboard")
 
+st.set_page_config(page_title = "Mail Extracts", layout = "wide")
+df = pd.read_csv("data/extract_mail.csv")
+
+st.dataframe(df, height=1500)
