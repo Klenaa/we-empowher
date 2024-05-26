0020c6b43b6060555a84a130156a5efb7e7de8fb
KLeennaa <klena.cheam@gmail.com>
2024-05-26 22:26:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ import numpy as np
 st.set_page_config(page_title="We Empowher", page_icon=":sparkles:", layout="centered", initial_sidebar_state="auto", menu_items=None)
 st.title("Dashboard")
 
-df = pd.read_csv("extract_mail.csv")
-st.dataframe(df)
+# df = pd.read_csv("extract_mail.csv")
+# st.dataframe(df)
