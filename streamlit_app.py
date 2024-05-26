de1293f71e61a5eab910a2daedcca47cd699d020
KLeennaa <klena.cheam@gmail.com>
2024-05-26 22:19:00 +0200

    csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st 
-import pandas as pde
+import pandas as pd
 import numpy as np
 
 st.title("Dashboard")
 
 st.set_page_config(page_title = "Mail Extracts", layout = "wide")
-df = pd.read_csv("data/extract_mail.csv")
+df = pd.read_csv("extract_mail.csv")
 
 st.dataframe(df, height=1500)
