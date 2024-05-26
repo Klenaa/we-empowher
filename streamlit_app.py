ef93d2523add93db4810c2bab20ba87ed5675363
KLeennaa <klena.cheam@gmail.com>
2024-05-26 22:19:58 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import numpy as np
 
 st.title("Dashboard")
 
-st.set_page_config(page_title = "Mail Extracts", layout = "wide")
 df = pd.read_csv("extract_mail.csv")
 
 st.dataframe(df, height=1500)
