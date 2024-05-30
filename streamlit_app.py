8ad3df2154b64ca0f28297ed16de2ac449683381
KLeennaa <klena.cheam@gmail.com>
2024-05-30 20:11:51 +0200

    csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,15 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-import openpyxl as ox
+
 
 
 st.set_page_config(page_title="We Empowher", page_icon=":sparkles:", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
-DATA_URL = ("data/extract_mail.xlsx")
+DATA_URL = ("data/extract_satisfaction_verbatim.csv")
 
 def load_data(nrows):
-    data = ox.read_excel(DATA_URL, nrows=nrows)
+    data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     return data
