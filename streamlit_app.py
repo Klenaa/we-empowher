6d9bcb0a00fed2ed465092fc868ca7458218b1b8
KLeennaa <klena.cheam@gmail.com>
2024-05-30 20:39:43 +0200

    app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,20 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-
+header = st.beta_container()
+dataset = st.beta_container()
+features = st.beta_container()
+modelTraining = st.beta_container()
 
 st.set_page_config(page_title="We Empowher", page_icon=":sparkles:", layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+with header:
+    st.title("We EmpowHer: Hackathon")
+    st.text("Dans le cadre du Hackathon")
+
+
+with dataset:
+    st.header("Verbatim Dataset")
 
 DATA_URL = ("data/extract_satisfaction_verbatim.csv")
 
@@ -20,3 +31,18 @@ data = load_data(10000)
 data_load_state.text('Loading data...done!')
 
 data
+
+location_dist = pd.DataFrame(data['satisfaction cpam'].value_counts())
+st.bar_chart(location_dist)
+
+st.markdown('*First*: i know')
+
+
+with model_training:
+    st.header("here is a header")
+
+    selection_column, disp_col = st.beta_columns(2)
+    max_depth = selection_column.slider("What should be the max", min_value = 10, max_value=100)
+    variable = selection_column.selectbox('How Many tree should be used ?', option = [100,200,300, "No Limits"])
+    input_feature = selection_column.text_input("question" )
+
